4acb9a822792a27fa07ab0da755e730719910088
Jobecoo <joanbertomeu@iesebre.com>
2024-11-24 21:02:15 +0100

    Add files via upload

---
+++ A5/script.js
@@ -0,0 +1,31 @@
+let startTime = Date.now();
+
+function updateTimer() {
+    let currentTime = Date.now();
+    let elapsedTime = Math.floor((currentTime - startTime) / 1000);
+    document.getElementById('timer').textContent = `Tiempo en la página: ${elapsedTime} segundos`;
+}
+
+setInterval(updateTimer, 1000);
+
+function showDescription(img) {
+    const description = img.nextElementSibling;
+    description.style.display = 'block';
+}
+
+function hideDescription(img) {
+    const description = img.nextElementSibling;
+    description.style.display = 'none';
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const thumbnails = document.querySelectorAll('.thumbnail');
+    const largeImage = document.getElementById('large-image');
+
+    thumbnails.forEach(thumbnail => {
+        thumbnail.addEventListener('click', function() {
+            largeImage.src = this.src;
+            largeImage.style.display = 'block';
+        });
+    });
+});
